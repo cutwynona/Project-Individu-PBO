06e6161f5f57f2e63f8c89502f2e34e6380d92ea
cutwynona <wynonacut@gmail.com>
2025-11-30 00:01:06 +0700

    Update AturanPenumpang..java

--- AturanPenumpang..java
+++ AturanPenumpang..java
@@ -1,1 +1,12 @@
+public class AturanPenumpang {
+    // Menentukan jenis penumpang: 
+    // PRIORITAS = lansia (>60), anak-anak (<10), atau ibu hamil
+    // BIASA = selain kriteria di atas
+    public static JenisPenumpang tentukan(int umur, boolean hamil) {
+        if (umur < 10 || umur > 60 || hamil) {
+            return JenisPenumpang.PRIORITAS;
+        }
+        return JenisPenumpang.BIASA;
+    }
+}
 
