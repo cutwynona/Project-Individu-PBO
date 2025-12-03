ddcf0a216fae03e9874fe40faac54ec9a1cb4553
cutwynona <wynonacut@gmail.com>
2025-12-03 08:52:02 +0700

    Add PenumpangPrioritas class for priority passengers

--- PenumpangPrioritas.java
+++ PenumpangPrioritas.java
@@ -1,1 +1,7 @@
+// Class untuk penumpang kategori PRIORITAS
+public class PenumpangPrioritas extends Penumpang {
+    public PenumpangPrioritas(int id, String nama, int umur, boolean hamil) {
+        super(id, nama, umur, hamil);
+    }
+}
 
