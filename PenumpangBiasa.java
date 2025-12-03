b9d139fba70406f9f9b8b701b82e824247b251ee
cutwynona <wynonacut@gmail.com>
2025-12-03 08:50:33 +0700

    Update PenumpangBiasa.java

--- PenumpangBiasa.java
+++ PenumpangBiasa.java
@@ -1,1 +1,7 @@
+// Class untuk penumpang kategori BIASA
+public class PenumpangBiasa extends Penumpang {
+    public PenumpangBiasa(int id, String nama, int umur, boolean hamil) {
+        super(id, nama, umur, hamil);
+    }
+}
 
