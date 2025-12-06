c8ef6a2286421b7137fa14f8b7b0aefd2cb263db
cutwynona <wynonacut@gmail.com>
2025-12-06 15:44:02 +0700

    Update SaldoTidakCukup.java

--- SaldoTidakCukup.java
+++ SaldoTidakCukup.java
@@ -1,1 +1,9 @@
+// Exception khusus untuk menandai bahwa saldo tidak mencukupi
+public class SaldoTidakCukup extends Exception {
+
+    // Meneruskan pesan error ke superclass Exception
+    public SaldoTidakCukup(String msg) {
+        super(msg);
+    }
+}
 
